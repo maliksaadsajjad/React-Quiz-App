66975d0b88d67d6963d6b02321d9eb8550bec02a
maliksaadsajjad <mr.aantojdz@gmail.com>
2026-01-01 18:38:31 +0500

    Outputting questions and Registering answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,32 @@
 import { useState } from 'react';
 
+import QUESTIONS from '../questions.js';
+
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
 
-  return <p>Currently active Question</p>;
+  function hanldeSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => hanldeSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
