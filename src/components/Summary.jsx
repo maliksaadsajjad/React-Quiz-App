f5e4a0902d68b49c29a447bfd5a1daf02303f5e3
maliksaadsajjad <mr.aantojdz@gmail.com>
2026-01-03 01:41:07 +0500

    Added the Quiz stats logic

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,21 +2,34 @@ import quizCompleteImg from '../assets/quiz-complete.png';
 import QUESTIONS from '../questions.js';
 
 export default function Summary({ userAnswers }) {
+  const skippedAnswers = userAnswers.filter((answer) => answer === null);
+  const correctAnswers = userAnswers.filter(
+    (answer, index) => answer === QUESTIONS[index].answers[0]
+  );
+
+  const skippedAnswersShare = Math.round(
+    (skippedAnswers.length / userAnswers.length) * 100
+  );
+  const correctAnswersShare = Math.round(
+    (correctAnswers.length / userAnswers.length) * 100
+  );
+  const wrongAnswersShare = 100 - skippedAnswersShare - correctAnswersShare;
+
   return (
     <div id="summary">
       <img src={quizCompleteImg} alt="Trophy Icon" />
       <h2>Quiz Completed!</h2>
       <div id="summary-stats">
         <p>
-          <span className="number">10%</span>
+          <span className="number">{skippedAnswersShare}%</span>
           <span className="text">skipped</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{correctAnswersShare}%</span>
           <span className="text">answered correctly</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{wrongAnswersShare}%</span>
           <span className="text">answered incorrectly</span>
         </p>
       </div>
